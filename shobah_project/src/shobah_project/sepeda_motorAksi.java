4d84d144b492466c7e529fa0afcdcd4d1afa66ec
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2018-03-24 22:40:43 +0800

    Mengisi Nilai Atribut

--- shobah_project/src/shobah_project/sepeda_motorAksi.java
+++ shobah_project/src/shobah_project/sepeda_motorAksi.java
@@ -14,6 +14,24 @@ public class sepeda_motorAksi {
         sepeda_motor honda = new sepeda_motor();
         sepeda_motor yamaha = new sepeda_motor();
         sepeda_motor suzuki = new sepeda_motor();
+        
+        honda.type="scoopy";
+        honda.merk="honda";
+        honda.warna="brown";
+        honda.harga="18950500";
+        honda.nopol="DA 5321 BCF";
+        
+        yamaha.type="NMX";
+        yamaha.merk="yamaha";
+        yamaha.warna="putih";
+        yamaha.harga="40000000";
+        yamaha.nopol="DA 6564 PBB";
+        
+        suzuki.type="GSX-R150";
+        suzuki.merk="suzuki";
+        suzuki.warna="merah hitam";
+        suzuki.harga="27500000";
+        suzuki.nopol="DA 6323 KAB";
     }
     
 }
