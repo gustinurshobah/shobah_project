7cd6c64109e4719c9439dcf42c6090aced646822
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2018-03-24 22:23:14 +0800

    Membuat Class Aksi dan Method main

---
+++ shobah_project/src/shobah_project/sepeda_motorAksi.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shobah_project;
+
+/**
+ *
+ * @author ASUS
+ */
+public class sepeda_motorAksi {
+    public static void main(String[] args) {
+        
+    }
+    
+}
